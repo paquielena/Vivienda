0cdc93cdad14df2df92eaf5994715e1a7fcace49
ShiroHaru99 <48929700+ShiroHaru99@users.noreply.github.com>
2023-03-13 14:32:42 +0100

    refactorización realizada
    Se realizaron los siguientes apartados de refactorización:
    - Cambia el nombre de la variable v por casa1.
    - Introduce el método imprimirVivienda, que englobe las sentencias de la clase Hogar que operan con el objeto casa1, añadiendo los parámetros necesarios.
    - Encapsula los atributos de la clase Vivienda añadiendo los setter y getter correspondientes. Los atributos deben ser privados y el acceso a ellos deberá ser a través de los setter y getter.
    - Crea un nuevo constructor de la clase  Vivienda que reciba todos los parámetros de la clase.
    - Modifica el método actualizarPrecio para que reciba un parámetro llamado  descuento , que sea el porcentaje a restar del precio actual.

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -11,21 +11,71 @@ package Hogar;
  */
 public class Vivienda {
     
-    double precio;
-    int numHabitaciones;
-    double superficie;
-    boolean parking;
-    String estado;
-    String propietario;
+    private double precio;
+    private int numHabitaciones;
+    private double superficie;
+    private boolean parking;
+    private String estado;
+    private String propietario;
+
+    //GETTERS
+    public double getPrecio() {
+        return precio;
+    }
+    public int getNumHabitaciones() {
+        return numHabitaciones;
+    }
+    public double getSuperficie() {
+        return superficie;
+    }
+    public boolean isParking() {
+        return parking;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public String getPropietario() {
+        return propietario;
+    }
+
+    //SETTERS
+    public final void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public final void setNumHabitaciones(int numHabitaciones) {
+        this.numHabitaciones = numHabitaciones;
+    }
+    public final void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+    public final void setParking(boolean parking) {
+        this.parking = parking;
+    }
+    public final void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public final void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
     
     public Vivienda (double precio)
     {
-        this.precio = precio;
+        this.setPrecio(precio);
+    }
+    public Vivienda(double pr, int n, double s, boolean p, String e, String prop){
+        this.setPrecio(pr);
+        this.setNumHabitaciones(n);
+        this.setSuperficie(s);
+        this.setParking(p);
+        this.setEstado(e);
+        this.setPropietario(prop);
     }
     
-    public void actualizarPrecio()
+    public void actualizarPrecio(double descuento)
     {
-        precio = precio - precio * 0.05;
+        this.setPrecio(precio - (precio * descuento));
     }
+
+    
     
 }
