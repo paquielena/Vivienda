60ab5c33509197682bc753b530e1278b2e99b49a
alumno <alumno@30010577F138B8>
2023-03-14 15:07:34 +0100

    refactorización realizada

--- src/Hogar/Hogar.java
+++ src/Hogar/Hogar.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Hogar;
 
 /**
@@ -14,8 +10,9 @@ public class Hogar {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Vivienda v = new Vivienda(120000);
+    public static void main(String[] args) 
+    {
+        Vivienda casa1 = new Vivienda(120000);
         
         double precio;
         int numHabitaciones;
@@ -24,12 +21,21 @@ public class Hogar {
         String estado;
         String propietario;
         
-        System.out.println("Precio: " + v.precio);
-        System.out.println("Número de habitaciones: "+ v.numHabitaciones);
-        System.out.println("Superficie en m2: " + v.superficie);
-        System.out.println("¿Dispone de parking?: " + v.parking);
-        System.out.println("Estado: "+ v.estado);
-        System.out.println("Propietario: "+ v.propietario);
+        imprimirvivienda(casa1);
+        
+        
     }
+
+    private static void imprimirvivienda(Vivienda casa1) 
+    {
+        System.out.println("Precio: " + casa1.getPrecio());
+        System.out.println("Número de habitaciones: "+ casa1.getNumHabitaciones());
+        System.out.println("Superficie en m2: " + casa1.getSuperficie());
+        System.out.println("¿Dispone de parking?: " + casa1.isParking());
+        System.out.println("Estado: "+ casa1.getEstado());
+        System.out.println("Propietario: "+ casa1.getPropietario());
+    }
+    
+    
     
 }
