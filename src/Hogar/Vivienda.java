69af1a672a917942ea899fe72e3f661032bd5361
alumno <alumno@30010577F138B8>
2023-03-14 14:59:12 +0100

    documentación realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -5,17 +5,34 @@
  */
 package Hogar;
 
-/**
+/**<h2>Este es el método Vivienda</h2> 
  *
- * @author paqui
+ * 
  */
 public class Vivienda {
-    
+/**
+* Este es el atributo precio de la vivienda. Tipo double
+*/
     private double precio;
+/**
+* Este es el atributo relacionado con el numero de Habitaciones de la vivienda. Tipo entero
+*/
     private int numHabitaciones;
+/**
+* Este es el atributo superficie de la vivienda. Tipo double
+*/
     private double superficie;
+/**
+* Este es el atributo parking, y nos dice si tiene o no parking. Tipo boolean
+*/
     private boolean parking;
+/**
+* Este es el atributo estado. Tipo String
+*/
     private String estado;
+/**
+* Este es el atributo propietario, y nos dice quien es el propietario de la vivienda. Tipo String
+*/
     private String propietario;
 
     public double getPrecio() {
@@ -65,7 +82,15 @@ public class Vivienda {
     public void setPropietario(String propietario) {
         this.propietario = propietario;
     }
-
+/** Constructor con 6 parametros
+ * 
+ * @param precio Precio de la vivienda
+ * @param numHabitaciones Numero de habitaciones de la vivienda
+ * @param superficie Superficie de la vivienda
+ * @param parking Tiene parking o no
+ * @param estado Estado de la vivienda
+ * @param propietario Propietario de la vivienda
+ */
     public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
         this.precio = precio;
         this.numHabitaciones = numHabitaciones;
@@ -78,12 +103,18 @@ public class Vivienda {
     
     
     
-    
+/**Nos da el precio de la vivienda
+ * @see Vivienda 
+ * @param precio de la vivienda
+ */    
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
-    
+/** Nos da el precio con descuento de la vivienda
+ * @see Vivienda
+ * @param descuento que se aplica al precio
+ */  
     public void actualizarPrecio(double descuento)
     {
         precio = precio - precio * descuento;
