544ec631a769870773faa6f05bdc77a8c2f16482
alumno <alumno@30010577F138B8>
2023-03-14 15:00:00 +0100

    documentación realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -6,22 +6,51 @@
 package Hogar;
 
 /**
- *
- * @author paqui
+ *<h2>Este es un comentario de Javadoc</h2>
+ * 
+ * @author Franklin Estrada
  */
 public class Vivienda {
-    
+    /**
+     * Atributo precio de la vivienda. Tipo double
+     */
     private double precio;
+    /**
+     * Atributo numHabitaciones de la vivienda. Tipo int
+     */
     private int numHabitaciones;
+    /**
+     * Atributo superficie de la vivienda. Tipo double
+     */
     private double superficie;
+    /**
+     * Atributo parking de la vivienda. Tipo boolean
+     */
     private boolean parking;
+    /**
+     * Atributo estado de la vivienda. Tipo string
+     */
     private String estado;
+    /**
+     * Atributo propietario de la vivienda. Tipo string
+     */
     private String propietario;
+    
+    /**
+     * Constructor con 1 parametro. 
+     * Crea objetos Vivienda con precio
+     * @param precio.Precio de la vivienda
+     */
     
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
+    /**
+     * Metodo actualizarPrecio con 1 parametro. 
+     * Actualiza el precio de la vivienda.
+     * @param descuento.Descuento que se le resta al precio de la vivienda
+     */
     
     public void actualizarPrecio(double descuento)
     {
@@ -75,6 +104,17 @@ public class Vivienda {
     public void setPropietario(String propietario) {
         this.propietario = propietario;
     }
+    
+     /**
+     * Constructor con 6 parametros.
+     * Crea objetos Vivienda con precio,numHabitaciones,superficie,parking,estado y propietario,.
+     * @param precio.Este es el precio
+     * @param numHabitaciones. Este es el numero de habitaciones
+     * @param superficie. Este es la superficie
+     * @param parking. Este es el parking
+     * @param estado. Este es el estado
+     * @param propietario. Este es el estado
+     */
 
     public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
         this.precio = precio;
