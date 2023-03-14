5159be8ba0c1a1dcd386d379999e02e9297464c5
alumno <alumno@30010577F138B8>
2023-03-14 14:39:11 +0100

    Documentación realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -14,75 +14,85 @@ public class Vivienda {
     /**
      * @return the precio
      */
+    
+    /**
+     * 
+     * Este método se encarga de obtener el atributo del precio y lo devuelve.
+     */
     public double getPrecio() {
         return precio;
     }
 
     /**
-     * @return the numHabitaciones
+     * Este método se encarga de obtener el atributo de NumHabitaciones y lo devuelve.
      */
     public int getNumHabitaciones() {
         return numHabitaciones;
     }
 
     /**
-     * @return the superficie
+     * Este método obtiene el atributo Superficie y lo obtiene.
      */
     public double getSuperficie() {
         return superficie;
     }
 
     /**
-     * @return the parking
+     * Este método obtiene el resultado de true o false del método Parking
      */
     public boolean isParking() {
         return parking;
     }
 
     /**
-     * @return the estado
+     * Este método obtiene el atributo de Estado y lo devuelve.
      */
     public String getEstado() {
         return estado;
     }
 
     /**
-     * @return the propietario
+     * Este método devuelve el atributo Propietario y lo obtiene.
      */
     public String getPropietario() {
         return propietario;
     }
 
     /**
-     * @param precio the precio to set
+     * @param precio es el precio a cambiar
+     * El método se encarga de cambiar el valor de precio
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
-     * @param numHabitaciones the numHabitaciones to set
+     * @param numHabitaciones el número de habitaciones a cambiar.
+     * Este método se encarga de cambiar el número de habitaciones.
      */
     public void setNumHabitaciones(int numHabitaciones) {
         this.numHabitaciones = numHabitaciones;
     }
 
     /**
-     * @param superficie the superficie to set
+     * @param superficie la superficie de la vivienda
+     * Este método cambia el valor que tenía la superficie
      */
     public void setSuperficie(double superficie) {
         this.superficie = superficie;
     }
 
     /**
-     * @param parking the parking to set
+     * @param parking nos dice si tiene parking o no.
+     * Se encarga de decirnos true o false el método.
      */
     public void setParking(boolean parking) {
         this.parking = parking;
     }
 
     /**
-     * @param estado the estado to set
+     * @param estado el estado en el que se encuentra la vivienda
+     * El método se encarga de cambiar el valor del estado.
      */
     public void setEstado(String estado) {
         this.estado = estado;
@@ -94,21 +104,44 @@ public class Vivienda {
     public void setPropietario(String propietario) {
         this.propietario = propietario;
     }
-    
+    /**
+     * @param precio Se trata de precio de la vivienda
+     */
     private double precio;
+    /**
+     * @param numHabitaciones Se trata del número de habitaciones de la vivienda.
+     */
     private int numHabitaciones;
+    /**
+     * @param superficie se trata de la superficie total de la vivienda.
+     */
     private double superficie;
+    /**
+     * @param parking se trata de un booleano que nos dice si hay parking o no.
+     */
     private boolean parking;
+    /**
+     * @param estado se trata del estado de la vivienda.
+     */
     private String estado;
+    /**
+     * @param propietario se trata del propietario de la vivienda.
+     */
     private String propietario;
     
+    /**
+     * 
+     * @param precio  el precio  de la vivienda
+     * se trata del constructor de la vivienda.
+     */
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
     /**
      * 
-     * @param descuento 
+     * @param descuento es el descuento a realizar a la compra
+     * se encarga de actualizar el precio para comprar una vivienda.
      */
     public void actualizarPrecio(double descuento)
     {
