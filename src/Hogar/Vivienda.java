a75bdb8910ca23d0d169392376967af2835f5efc
Lorena <Lorena@LorenaPC>
2023-03-14 19:24:56 +0100

    Documentacion realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -6,8 +6,18 @@
 package Hogar;
 
 /**
- *
+ ** <h2> Esta es una clase que se llama Vivienda y pertenece al programa Vivienda</h2>
+ *En este caso, la clase Vivienda tiene una serie de elementos:
+ * <ul>
+ * <li><b> atributos</b> </li>
+ *  <li><b> dos constructores</b> </li>
+ *  <li><b> Getter y Setter</b> </li>
+ *  <li><b> un metodo</b> </li>
+ * </ul>
+ * Asignatura Entornos de desarrollo del Ciclo de Daw de <a href= https://www.iesjoseplanes.es/> Ies Jose Planes</a>
+ * @version 1.1
  * @author paqui
+ * @since 1.0
  */
 public class Vivienda {
 
@@ -94,14 +104,42 @@ public class Vivienda {
     public void setPropietario(String propietario) {
         this.propietario = propietario;
     }
-    
+    /**
+     * Atributo precio de la vivienda. Tipo double
+     */
     private double precio;
+    /**
+     * Atributo numero de habitaciones. Tipo entero
+     */
     private int numHabitaciones;
+    /**
+     * Atributo superficie de la Vivienda. Tipo real
+     */
     private double superficie;
+    /**
+     * Atributo que indica la existencia de parking o no. Tipo booleano
+     */
     private boolean parking;
+    /**
+     * Atributo que indica el estado de la Vivienda. Tipo candena
+     */
     private String estado;
+    /**
+     * Atributo nombre del propietario de la Vivienda. Tipo candena
+     */
     private String propietario;
 
+    /**
+     * Constructor con 6 parametros
+     * Crea objetos Vivienda con precio, Numero de Habitaciones, superficio, parking,
+     * estado y propietario
+     * @param precio Precio de la Vivienda
+     * @param numHabitaciones Numero de habitaciones de las que dispone
+     * @param superficie Superficie en metros cuadrados de la vivienda
+     * @param parking Indicador de parking incluido o no
+     * @param estado Estado de la vivienda
+     * @param propietario Nombre del propietario actual
+     */
     public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
         this.precio = precio;
         this.numHabitaciones = numHabitaciones;
@@ -111,14 +149,21 @@ public class Vivienda {
         this.propietario = propietario;
     }
     
-    
-    
-    
+    /**
+     * Constructor con 1 parametros
+     * Crea objetos Vivienda con precio
+     * @param precio Precio de la Vivienda
+     */
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
     
+    /**
+     * Esta método inserta el descuento a aplicar en el precio de la vivienda actual
+     * @see Vivienda
+     * @param descuento int que almacena el descuento a aplizar al precio de la Vivienda
+     */
     public void actualizarPrecio(int descuento)
     {
         setPrecio(getPrecio() - getPrecio() * descuento/100);
