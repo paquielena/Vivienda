4110683324952e685fb6659be3fe0d1e1933d379
xMini23 <sergio.mrntz@gmail.com>
2023-03-16 12:55:11 +0100

    Documentación Realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -7,17 +7,40 @@ package Hogar;
 
 /**
  *
- * @author paqui
+ * @author Sergio
+ * @version: UPDATE FROM PAQUI.
+ * 
  */
 public class Vivienda {
 
-
+/**
+ *<p> En la clase Vivienda tenemos distintos tipos de atributos que se le pueden acreditar para tener mas información sobre la vivienda </p>
+ * 
+ */
     
+    /**
+     * <p>Nos indica el precio de la vivienda de tipo real </p>
+     */
     private double precio;
+      /**
+     * <p> Nos indica los numeros de habitaciones de tipo entero </p>
+     */
     private int numHabitaciones;
+          /**
+     * <p>Nos indica la superficie de tipo real  de la casa </p>
+     */
     private double superficie;
+             /**
+     * <p>Nos indica si la casa tiene parking  de tipo boolean </p>
+     */
     private boolean parking;
+                 /**
+     * <p>Nos indica el estaod de la casa de tipo String  </p>
+     */
     private String estado;
+                  /**
+     * <p>Nos indica el propietario de la casa de tipo String  </p>
+     */
     private String propietario;
             
      public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
@@ -33,12 +56,21 @@ public class Vivienda {
         this.precio = precio;
     }
     
-    public void actualizarPrecio(int descuento, Object par1)
+    
+    /**
+     * 
+     * <p> Nos indica el precio aplicandole el descuento que se le indica en el parametro <b> descuento </b>  </p>
+     */
+    public void actualizarPrecio(int descuento)
     {
         setPrecio(getPrecio() - (getPrecio() * descuento / 100));
     
     }
-    
+  
+    /**
+     * <p>  Imprime los valores de una vivienda para que puedas visualizarlo por <b> pantalla </b> </p>
+     *
+     */
     public void imprimirVivienda()
     {
           System.out.println("Precio: " + this.getPrecio());
@@ -50,84 +82,84 @@ public class Vivienda {
     }
 
     /**
-     * @return the precio
+     * <p>  Imprime los valores del precio de una vivienda  para que puedas visualizarlo por <b> pantalla </b> </p>
      */
     public double getPrecio() {
         return precio;
     }
 
     /**
-     * @param precio the precio to set
+     * <p>  Inserta los valores del atributo <b> precio </b> </p>
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
-     * @return the numHabitaciones
+     * <p>  Imprime los valores del numero de habitaciones de una vivienda  para que puedas visualizarlo por <b> pantalla </b> </p>
      */
     public int getNumHabitaciones() {
         return numHabitaciones;
     }
 
     /**
-     * @param numHabitaciones the numHabitaciones to set
+     * <p>  Inserta los valores del atributo <b> numero de habitaciones </b> </p>
      */
     public void setNumHabitaciones(int numHabitaciones) {
         this.numHabitaciones = numHabitaciones;
     }
 
     /**
-     * @return the superficie
+     * <p>  Imprime los valores del numero de metros cuadrados de superficie  de una vivienda  para que puedas visualizarlo por <b> pantalla </b> </p>
      */
     public double getSuperficie() {
         return superficie;
     }
 
     /**
-     * @param superficie the superficie to set
+     * <p>  Inserta los valores del atributo <b> superficie </b> </p>
      */
     public void setSuperficie(double superficie) {
         this.superficie = superficie;
     }
 
     /**
-     * @return the parking
+     * <p>  Imprime los valores de si tiene o no la vivienda  <b> PARKING </b> </p>
      */
     public boolean isParking() {
         return parking;
     }
 
     /**
-     * @param parking the parking to set
+     * <p>Inserta si tiene o no  <b> parking la vivienda </b> </p>
      */
     public void setParking(boolean parking) {
         this.parking = parking;
     }
 
     /**
-     * @return the estado
+     * <p>Obtinne el estado  <b> de la vivienda </b> </p>
      */
     public String getEstado() {
         return estado;
     }
 
     /**
-     * @param estado the estado to set
+     * <p>Inserta el estado <b> de  la vivienda </b> </p>
      */
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
     /**
-     * @return the propietario
+     * <p>Obtiene <b> el nombre del propietario de la vivienda  </b> </p>
      */
     public String getPropietario() {
         return propietario;
     }
 
     /**
-     * @param propietario the propietario to set
+     * <p>Inserta el nombre del  <b> propietario de la vivienda </b> </p>
      */
     public void setPropietario(String propietario) {
         this.propietario = propietario;
