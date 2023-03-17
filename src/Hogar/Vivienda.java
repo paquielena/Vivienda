79b670a99607aa6501265e6acd0e86225a43c027
Saro <Saro@DESKTOP-PJHRJGE>
2023-03-17 17:41:13 +0100

    Documentación realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -6,102 +6,156 @@
 package Hogar;
 
 /**
- *
- * @author paqui
+ * <h2>Documentación de la clase Vivienda</h2>
+ * Esta clase contiene una serie de parámetros 
+ * relacionados con una vivienda.
+ * También contiene un único método <b><i>actualizarPrecio</i></b> que aplica un descuento al Precio.
+ * Estos parámetros se introducen en un sólo constructor.
+ * @author Alberto-González
+ * @version v1.0
  */
 public class Vivienda {
 
     /**
-     * @return the precio
+     * Devuelve el Precio - Tipo double
+     * @return precio
      */
     public double getPrecio() {
         return precio;
     }
 
     /**
-     * @param precio the precio to set
+     * Cambia el precio - Tipo double
+     * @param precio El precio de la vivienda
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
-     * @return the numHabitaciones
+     * Devuelve el número de habitaciones - Tipo int
+     * @return numHabitaciones
      */
     public int getNumHabitaciones() {
         return numHabitaciones;
     }
 
     /**
-     * @param numHabitaciones the numHabitaciones to set
+     * Cambia el número de habitaciones - Tipo int
+     * @param numHabitaciones El número de habitaciones
      */
     public void setNumHabitaciones(int numHabitaciones) {
         this.numHabitaciones = numHabitaciones;
     }
 
     /**
-     * @return the superficie
+     * Devuelve la superficie en metros cuadrados - Tipo double
+     * @return superficie
      */
     public double getSuperficie() {
         return superficie;
     }
 
     /**
-     * @param superficie the superficie to set
+     * Cambia la superficie en metros cuadrados - Tipo double
+     * @param superficie La superficie de la vivienda
      */
     public void setSuperficie(double superficie) {
         this.superficie = superficie;
     }
 
     /**
-     * @return the parking
+     * Devuelve true o false dependiendo de si hay parking o no - Tipo boolean
+     * @return parking
      */
     public boolean isParking() {
         return parking;
     }
 
     /**
-     * @param parking the parking to set
+     * Cambia el valor que define si hay Parking(True) o no (False) - Tipo boolean
+     * @param parking Si la vivienda tiene Parking o no (True/False)
      */
     public void setParking(boolean parking) {
         this.parking = parking;
     }
 
     /**
-     * @return the estado
+     * Devuelve una descripción del estado de la vivienda - Tipo cadena
+     * @return estado
      */
     public String getEstado() {
         return estado;
     }
 
     /**
-     * @param estado the estado to set
+     * Cambia la descripción del estado de la vivienda - Tipo cadena
+     * @param estado El estado de la vivienda
      */
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
     /**
-     * @return the propietario
+     * Devuelve el nombre del propietario - Tipo cadena
+     * @return propietario
      */
     public String getPropietario() {
         return propietario;
     }
 
     /**
-     * @param propietario the propietario to set
+     * Cambia el nombre del propietario - Tipo cadena
+     * @param propietario El propietario de la vivienda
      */
     public void setPropietario(String propietario) {
         this.propietario = propietario;
     }
-    
+    /**
+     * Atributo precio de la vivienda - Tipo double
+     */
     private double precio;
+    /**
+     * Número de habitaciones - Tipo int
+     */
     private int numHabitaciones;
+    /**
+     * La superfície. Se interpreta en metros cuadrádos - Tipo double.
+     */
     private double superficie;
+    /**
+     * Si la vivienda tiene Parking o no.
+     *  - Tipo boolean
+     * <ul>
+     *  <li><b>True: Tiene parking.</b></li>
+     *  <li><b>False: No tiene parking.</b></li>
+     * </ul>
+     */
     private boolean parking;
+    /**
+     * La descripción del estado de la Vivienda - Tipo Cadena.
+     */
     private String estado;
+    /**
+     * El nombre completo del propietario - Tipo Cadena.
+     */
     private String propietario;
 
+    /**
+     * Método constructor con tódos los <b>seis</b> parámetros de la clase.
+     * Crea el objeto vivienda con precio, número de habitaciones, superficie, si tiene parking o no, estado de la vivienda y propietario.
+     * @param precio Hace referencia al Precio de la vivienda - Tipo double.
+     * @param numHabitaciones Hace referncia al número de habitaciones - Tipo int.
+     * @param superficie La superfícia. Se interpreta en metros cuadrádos - Tipo double.
+     * @param parking Si la vivienda tiene Parking o no.
+     *  - Tipo boolean
+     * <ul>
+     *  <li><b>True: Tiene parking.</b></li>
+     *  <li><b>False: No tiene parking.</b></li>
+     * </ul>
+     * @param estado Da una descripción del estado de la Vivienda - Tipo Cadena.
+     * @param propietario El nombre completo del propietario - Tipo Cadena.
+     */
     public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
         this.precio = precio;
         this.numHabitaciones = numHabitaciones;
@@ -111,11 +165,22 @@ public class Vivienda {
         this.propietario = propietario;
     }
     
+    /**
+     * Método constructor que solo pide el precio como parámetro
+     * Tras la última refactorización este método no es particularmente útil.
+     * @param precio
+     */
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
     
+    /**
+     * Aplica un descuento porcentual al total del precio.
+     * @see Vivienda
+     * @param descuento de tipo double que se multiplicará al precio para dar el total del descuento y después se restará al total.
+     * Ej. 0.1 para un descuento del 10%.
+     */
     public void actualizarPrecio(double descuento)
     {
         setPrecio(getPrecio() - getPrecio() * descuento);
