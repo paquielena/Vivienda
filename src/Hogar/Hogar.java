6e571cbc82a68f8794e0a599b236ba62def4d132
thorcazo <thorcazodesign@gmail.com>
2023-03-17 20:05:30 +0100

    Refaztorizacion Realizada

--- src/Hogar/Hogar.java
+++ src/Hogar/Hogar.java
@@ -1,35 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Hogar;
 
 /**
+ * Clase principal Hogar que se encarga de gestionar las viviendas.
  *
- * @author paqui
+ * @author David Pastor
  */
 public class Hogar {
 
+
+
     /**
-     * @param args the command line arguments
+     * Método principal (main) que ejecuta la aplicación.
+     *
+     * @param args argumentos de la línea de comandos
      */
     public static void main(String[] args) {
-        Vivienda v = new Vivienda(120000);
-        
+        Vivienda casa1 = new Vivienda(120000);
+
         double precio;
         int numHabitaciones;
         double superficie;
         boolean parking;
         String estado;
         String propietario;
-        
-        System.out.println("Precio: " + v.precio);
-        System.out.println("Número de habitaciones: "+ v.numHabitaciones);
-        System.out.println("Superficie en m2: " + v.superficie);
-        System.out.println("¿Dispone de parking?: " + v.parking);
-        System.out.println("Estado: "+ v.estado);
-        System.out.println("Propietario: "+ v.propietario);
+
+        imprimirVivienda(casa1);
     }
-    
+
+    /**
+     * Método que imprime en consola los detalles de una vivienda dada.
+     *
+     * @param casa1 objeto Vivienda a imprimir
+     */
+    public static void imprimirVivienda(Vivienda casa1) {
+        System.out.println("Precio: " + casa1.getPrecio());
+        System.out.println("Número de habitaciones: " + casa1.getNumHabitaciones());
+        System.out.println("Superficie en m2: " + casa1.getSuperficie());
+        System.out.println("¿Dispone de parking?: " + casa1.isParking());
+        System.out.println("Estado: " + casa1.getEstado());
+        System.out.println("Propietario: " + casa1.getPropietario());
+    }
+
 }
