a2bf849a1c7f8f34f5ec83e2dcf88e77ba078c48
Gonzalo <>
2023-03-18 13:38:43 +0100

    refactorización realizada

--- src/Hogar/Hogar.java
+++ src/Hogar/Hogar.java
@@ -9,12 +9,14 @@ package Hogar;
  *
  * @author paqui
  */
-public class Hogar {
+public class Hogar 
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Vivienda casa1 = new Vivienda(120000);
         
         double precio;
@@ -23,13 +25,26 @@ public class Hogar {
         boolean parking;
         String estado;
         String propietario;
+              
         
-        System.out.println("Precio: " + casa1.precio);
-        System.out.println("Número de habitaciones: "+ casa1.numHabitaciones);
-        System.out.println("Superficie en m2: " + casa1.superficie);
-        System.out.println("¿Dispone de parking?: " + casa1.parking);
-        System.out.println("Estado: "+ casa1.estado);
-        System.out.println("Propietario: "+ casa1.propietario);
+//        System.out.println("Precio: " + casa1.precio);
+//        System.out.println("Número de habitaciones: "+ casa1.numHabitaciones);
+//        System.out.println("Superficie en m2: " + casa1.superficie);
+//        System.out.println("¿Dispone de parking?: " + casa1.parking);
+//        System.out.println("Estado: "+ casa1.estado);
+//        System.out.println("Propietario: "+ casa1.propietario);
+        
+        imprimirVivienda(casa1);
+    }
+    
+    public static void imprimirVivienda(Vivienda casa1)
+    {
+        System.out.println("Precio: " + casa1.getPrecio());
+        System.out.println("Número de habitaciones: "+ casa1.getNumHabitaciones());
+        System.out.println("Superficie en m2: " + casa1.getSuperficie());
+        System.out.println("¿Dispone de parking?: " + casa1.getParking());
+        System.out.println("Estado: "+ casa1.getEstado());
+        System.out.println("Propietario: "+ casa1.getPropietario());
     }
     
 }
